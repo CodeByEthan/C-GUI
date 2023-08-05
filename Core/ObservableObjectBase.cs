61c951c2cf45e56d8a048a5da9cd593790d3b087
HayEthan <ethan_296@live.com>
2023-08-05 21:01:38 +1000

    Initial Project Creation

---
+++ Core/ObservableObjectBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace WPF_flatUI.Core
+{
+    internal class ObservableObjectBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
